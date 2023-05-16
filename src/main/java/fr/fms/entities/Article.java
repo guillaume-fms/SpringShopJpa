0283350d95ac503ee97abd2f0d2c42b6f80f3df6
Guillaume <guillaume.pastor@fms-ea.com>
2023-05-16 15:12:04 +0200

    create project with Spring and Maven

---
+++ src/main/java/fr/fms/entities/Article.java
@@ -0,0 +1,65 @@
+package fr.fms.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Article implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private String brand;
+	private double price;
+	
+	
+	// Constructeur
+	public Article(String description, String brand, double price) {
+		this.description = description;
+		this.brand = brand;
+		this.price = price;
+	}
+
+	
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+	
+
+}
---
+++ src/main/java/fr/fms/entities/Category.java
@@ -0,0 +1,27 @@
+package fr.fms.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Category {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
