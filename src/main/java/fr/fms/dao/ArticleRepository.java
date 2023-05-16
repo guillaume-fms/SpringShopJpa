3fda99fb9389a2af89adaa06556dca5c0d59399c
Guillaume <guillaume.pastor@fms-ea.com>
2023-05-16 16:18:55 +0200

    create db, tables

---
+++ src/main/java/fr/fms/dao/ArticleRepository.java
@@ -0,0 +1,8 @@
+package fr.fms.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import fr.fms.entities.Article;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
+
+}
--- src/main/java/fr/fms/entities/Category.java
+++ src/main/java/fr/fms/entities/Category.java
@@ -1,13 +1,27 @@
 package fr.fms.entities;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class Category {
+
+@Entity
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+
+	// Constructeur
+	public Category(String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
 	
 	// Getters and Setters
 	public Long getId() {
